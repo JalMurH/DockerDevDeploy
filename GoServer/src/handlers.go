b03303d1261e49cddc16a7759f138d586ac0a3f5
JalMurH <jorge0420@outlook.es>
2023-06-17 14:50:28 -0500

    JSOn parse to use

--- GoServer/src/handlers.go
+++ GoServer/src/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -10,4 +11,32 @@ func HaldleRoot(w http.ResponseWriter, req *http.Request) {
 }
 func HandleHome(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Home end-point")
+}
+
+func PostRequest(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var metaData MetaData
+	err := decoder.Decode(&metaData)
+	if err != nil {
+		fmt.Fprintf(w, "Error %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "PayLoad %v\n", metaData)
+}
+
+func UserPostRequest(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Error %v\n", err)
+		return
+	}
+	response, err := user.ToJSON()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Conten-Type", "aplications/json")
+	w.Write(response)
 }
--- GoServer/src/main.go
+++ GoServer/src/main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("GET", "/", HaldleRoot)
+	server.Handle("POST", "/create", PostRequest)
+	server.Handle("POST", "/user", UserPostRequest)
 	server.Handle("POST", "/home", server.AddMiddlewares(HandleHome, CheckAuth(), Logging()))
 	server.Listen()
 }
